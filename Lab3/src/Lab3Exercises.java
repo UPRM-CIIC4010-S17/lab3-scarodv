072ec1352979a98f79eaa881ff26c06d03db9cbc
scarO <scarO@10.31.61.239>
2017-03-02 15:42:44 -0400

    Boricua

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -2,10 +2,10 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame ricardoReyes = new JFrame("Hello, world!");
+		JFrame ricardoReyes = new JFrame("Boricua!");
 		ricardoReyes.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		ricardoReyes.setLocation(900, 400);
-		ricardoReyes.setSize(200, 200);
+		ricardoReyes.setSize(300, 200);
 		
 		MyPanelClass myPanel = new MyPanelClass();
 		ricardoReyes.getContentPane().add(myPanel);
